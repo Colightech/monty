81dd4a866c1a9a351976cfdd8da310f876de74f4
Colightech <saintchris4@gmail.com>
2023-10-19 04:39:15 -0700

    stack and queue project

--- stack_functions.c
+++ stack_functions.c
@@ -131,7 +131,8 @@ void f_swap(stack_t **head, unsigned int counter)
 }
 
 /**
- * f_add - function taht adds the top two elements of the stack.
+ * f_add - function taht adds the
+ *top two elements of the stack.
  * @head: stack head
  * @counter: line_number
  * Return: void
